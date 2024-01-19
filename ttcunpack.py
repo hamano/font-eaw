b46b20031f2a192a6a1f87d9a80392a8edf4e1a9
HAMANO Tsukasa <code@cuspy.org>
2024-01-20 05:39:12 +0900

    fix lint

--- ttcunpack.py
+++ ttcunpack.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import sys
 import fontforge
-import json
 
 OUT_DIR='src/iosevka/'
 
 def main():
-    stats = {}
     path = sys.argv[1]
     names = fontforge.fontsInFile(path)
     for name in names:
