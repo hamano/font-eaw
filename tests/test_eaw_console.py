c8f1eba969b31b13e9310dfbf591ff041d5a5f9e
HAMANO Tsukasa <code@cuspy.org>
2024-01-22 09:50:59 +0900

    update test

--- tests/test_eaw_console.py
+++ tests/test_eaw_console.py
@@ -1,6 +1,6 @@
 from util import check_width
 
 def test_eaw_console():
-    err = check_width('../locale-eaw/dist/UTF-8-EAW-CONSOLE', 'build/EAW-CONSOLE-Regular.ttf')
+    err = check_width('locale/UTF-8-EAW-CONSOLE', 'build/EAW-CONSOLE-Regular.ttf')
     assert err == 0
 
