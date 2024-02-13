1444e547cef773a2d5d539893d4148cf98768d46
HAMANO Tsukasa <code@cuspy.org>
2024-02-13 14:57:19 +0900

    add subcommand

--- ft.py
+++ ft.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 import sys
+import click
 import fontforge
 from fontTools.ttLib import TTFont
 
-def list_glyph():
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('filename')
+def list_glyph(filename):
     path = sys.argv[1]
-    font = fontforge.open(path)
+    font = fontforge.open(filename)
     for g in font.glyphs():
         if g.unicode == 0:
             continue
@@ -14,6 +21,32 @@ def list_glyph():
         except ValueError:
             c = '?'
         print(f'{g.glyphname} {g.originalgid} U+{g.unicode:04X} {g.width} {c}')
+
+
+@cli.command()
+@click.argument('filename')
+def list_wwid(filename):
+    font = TTFont(filename)
+    gsub = font['GSUB'].table
+    index = None
+    for feature in gsub.FeatureList.FeatureRecord:
+        if feature.FeatureTag == 'WWID':
+            index = feature.Feature.LookupListIndex[0]
+    if index == None:
+        print('WWID LookupIndex notfound')
+        return None
+    lookup_wwid = gsub.LookupList.Lookup[index]
+    mapping = lookup_wwid.SubTable[0].ExtSubTable.mapping
+    cmap = font.getBestCmap()
+    rmap = {v: k for k, v in cmap.items()}
+    #print(rmap)
+    for n_name, w_name in mapping.items():
+        code = rmap.get(n_name, -1)
+        try:
+            c = chr(code)
+        except:
+            c = ''
+        print(f'U+{code:04X} {c} {n_name} -> {w_name}')
 
 def list_cmap():
     path = sys.argv[1]
@@ -42,6 +75,6 @@ def list_fwid():
         print(name, hmtx[name], new_name, hmtx[new_name])
 
 def main():
-    list_glyph()
+    cli()
 
 main()
