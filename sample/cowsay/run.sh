1fd39d63b53ae4d1c513136a735c9a7dfcce7694
HAMANO Tsukasa <code@cuspy.org>
2024-02-29 02:06:44 +0900

    add cowsay script

---
+++ sample/cowsay/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+
+TEMP_DIR=$(mktemp -d)
+
+function cowsay_file(){
+  filename=$1
+  cat $filename | cowsay -n > ${TEMP_DIR}/${filename}
+  offset=$((14 - $(wc -l < ${TEMP_DIR}/${filename})))
+  clear
+  for i in $(seq $offset); do
+    echo
+  done
+  cat ${TEMP_DIR}/${filename}
+  echo
+  sleep 0.1
+  import \
+    -window $(xdotool getactivewindow) \
+    -crop 800x480+0+0 \
+    "${TEMP_DIR}/$1.gif"
+}
+
+for f in *.txt; do
+  if [[ "$f" == _* ]]; then
+	continue
+  fi
+  cowsay_file $f
+done
+
+convert -loop 0 -delay 200 +repage ${TEMP_DIR}/*.gif cowsay.gif
+
+rm -rf ${TEMP_DIR}
