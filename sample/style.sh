e924ad72c9d3a6b730c28a12aa1027044de43739
HAMANO Tsukasa <code@cuspy.org>
2024-04-13 05:31:07 +0900

    update style.sh

--- sample/style.sh
+++ sample/style.sh
@@ -7,6 +7,13 @@ function print_style() {
     echo -e "[BoldItalic] \e[1m\e[3m$1\e[23m\e[22m"
 }
 
+clear
 print_style "Linuxでコンピューターの世界が広がります。1234566780"
-print_style "The quick brown fox jumps over the lazy dog"
-print_style "Grumpy wizards make toxic brew for the evil Queen and Jack."
+#print_style "The quick brown fox jumps over the lazy dog"
+#print_style "Grumpy wizards make toxic brew for the evil Queen and Jack."
+#
+sleep 0.1
+import \
+    -window $(xdotool getactivewindow) \
+    -crop 920x130+0+0 \
+    style.png
