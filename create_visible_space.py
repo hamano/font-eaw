99bbbafd29fc63f77d1bff83712d2f5df17cdb40
HAMANO Tsukasa <code@cuspy.org>
2024-11-07 15:52:36 +0900

    視える全角スペースのメトリクス調整

--- create_visible_space.py
+++ create_visible_space.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 import fontforge
+import psMat
 from pprint import pprint
+
+ASCENT = 1648
+DESCENT = 400
 
 def main():
     font_new = fontforge.font()
     font_new.encoding = "UnicodeFull"
     font_new.em = 2048
-    font_new.ascent = 1802
-    font_new.descent = 246
+    font_new.ascent = ASCENT
+    font_new.descent = DESCENT
     glyph = font_new.createChar(-1, "grid")
     glyph.width = 2024
     pen = glyph.glyphPen()
     width = 200
-    offset_x = 300
+    offset_x = 500
     offset_y = 100
+
     pen.moveTo((0, offset_y))
     pen.lineTo((2048, offset_y))
-    pen.lineTo((2048, font_new.ascent - font_new.descent - offset_y))
-    pen.lineTo((0, font_new.ascent - font_new.descent - offset_y))
+    pen.lineTo((2048, ASCENT - DESCENT - offset_y))
+    pen.lineTo((0, ASCENT - DESCENT - offset_y))
     pen.closePath()
 
-    pen.moveTo((offset_x, -200))
-    pen.lineTo((2048 - offset_x, -200))
-    pen.lineTo((2048 - offset_x, 2048))
-    pen.lineTo((offset_x, 2048))
+    pen.moveTo((offset_x, -DESCENT))
+    pen.lineTo((2048 - offset_x, -DESCENT))
+    pen.lineTo((2048 - offset_x, ASCENT))
+    pen.lineTo((offset_x, ASCENT))
     pen.closePath()
 
 # grid
@@ -41,13 +46,15 @@ def main():
 
     glyph.removeOverlap()
     font_ja = fontforge.open('src/bizudgothic/BIZUDGothic-Regular.ttf')
+    glyph = font_ja['glyph13070']
+    glyph.transform(psMat.translate(0, -100))
     font_ja.selection.select('glyph13070')
     font_ja.copy()
     font_new.selection.select('grid')
     font_new.copy()
     font_new.selection.select(0x3000)
     font_new.paste()
-    font_new[0x3000].exclude(font_ja['glyph13070'].layers[1])
+    font_new[0x3000].exclude(glyph.layers[1])
     font_new.generate('src/custom/visible_space.ttf')
 
 main()
